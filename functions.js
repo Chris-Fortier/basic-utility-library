72b5ae37521f748c3b569655cff4cd9d6bc091d5
Chris-Fortier <vendor@chrisjfortier.com>
2020-05-12 10:34:06 -0700

    add four more functions

--- functions.js
+++ functions.js
@@ -4,3 +4,24 @@ function add(input1, input2) {
    // R: a single JavaScript value
    return input1 + input2;
 }
+
+function subtract(input1, input2) {
+   // subtracts input2 from input1
+   return input1 - input2;
+}
+
+function multiply(input1, input2) {
+   // multiplies input1 by input 2
+   return input1 * input2;
+}
+
+function divide(input1, input2) {
+   // divides input1 by input 2
+   return input1 / input2;
+}
+
+function increment(input) {
+   // increments input by 1
+   input++;
+   return input;
+}
